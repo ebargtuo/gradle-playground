fdf79986d23ae4a090efd2255f1391fb84940228
ebargtuo <ebargtuo@gmx.it>
2015-09-05 11:06:17 +0200

    Initial commit with basic gradle buildSrc plugin

---
+++ hello-world-plugin-basic/buildSrc/src/main/java/hello/HelloWorldTask.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class HelloWorldTask extends DefaultTask {
+
+    @TaskAction
+    public void doHelloWorld() {
+        System.out.println("Hello world!");
+    }
+
+}
